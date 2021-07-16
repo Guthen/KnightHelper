8f17cc249fed53dca0464948b2b762853fd2a5ae
RinKaoru <46729658+RinKaoru@users.noreply.github.com>
2021-07-16 19:49:52 +0200

    advance animated player (not work)

--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
-var speed = 100
-
+var speed = 40
 
 func _ready():
 	pass 
@@ -9,13 +8,19 @@ func _physics_process( delta ):
 	var dir = Vector2()
 	
 	if Input.is_action_pressed( "up" ):
+		$AnimatedSprite.play("Walk")
 		dir.y -= 1
 	if Input.is_action_pressed( "down" ):
+		$AnimatedSprite.play("Walk")
 		dir.y += 1
 	if Input.is_action_pressed( "left" ):
+		$AnimatedSprite.play("Walk")
 		dir.x -= 1 
 	if Input.is_action_pressed( "right" ):
+		$AnimatedSprite.play("Walk")
 		dir.x += 1
+	else: 
+		$AnimatedSprite.stop()
 	
 	if dir == Vector2.ZERO:
 		return
