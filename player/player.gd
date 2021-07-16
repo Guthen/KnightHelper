a2c85ff93272a1c79e7a42582c4cd42e4cd40fc5
RinKaoru <46729658+RinKaoru@users.noreply.github.com>
2021-07-16 15:35:52 +0200

    modif dir

--- player/player.gd
+++ player/player.gd
@@ -1,20 +1,18 @@
-extends Node2D
+extends KinematicBody2D
+var speed = 100
 
 
 func _ready():
 	pass 
 
-func _process( delta ):
-	var right = Input.is_action_just_pressed( "ui_right" )
-	var left = Input.is_action_just_pressed( "ui_left" )
-	var up = Input.is_action_just_pressed( "ui_up" )
-	var down = Input.is_action_just_pressed( "ui_down" )
-	
-	if right:
-		position.x += 16
-	elif left:
-		position.x -= 16
-	elif up:
-		position.y -= 16
-	elif down:
-		position.y += 16
+func _physics_process( delta ):
+	var dir = Vector2()
+	if Input.is_action_pressed( "up" ):
+		dir.y -= 1
+	if Input.is_action_pressed( "down" ):
+		dir.y += 1
+	if Input.is_action_pressed( "left" ):
+		dir.x -= 1 
+	if Input.is_action_pressed( "right" ):
+		dir.x += 1
+	move_and_slide( dir.normalized() * speed )
