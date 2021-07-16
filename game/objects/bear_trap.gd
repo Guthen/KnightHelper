09c52bb177244265411fb6ab0ce3c400479c8bb2
Guthen <33220603+Guthen@users.noreply.github.com>
2021-07-16 22:54:00 +0200

    Player Damage + Bear Trap + Bomb Image

--- game/objects/bear_trap.gd
+++ game/objects/bear_trap.gd
@@ -1,12 +1,22 @@
 extends Node2D
 
+var is_activated = false
 
 func _ready():
 	pass 
 
 
 func _on_Area2D_body_entered( body ):
+	if is_activated:
+		return
+	
 	if body and body.layers == 2:
+		is_activated = true
 		$AnimatedSprite.set_frame( 1 )
+		
+		body.position = position + Vector2( 0, -10 )
 		body.is_freezed = true
-		body.position = position + Vector2( 0, -4 )
+		body.take_damage( 40 )
+		
+		yield( get_tree().create_timer( 1 ), "timeout" )
+		body.is_freezed = false
--- player/player.gd
+++ player/player.gd
@@ -1,14 +1,25 @@
 extends KinematicBody2D
 
+export var health: float = 100
+export var max_health: float = 100
 export var speed: int = 40
 var is_freezed: bool = false
 
 onready var game = get_node( "/root/Game" )
 
 func _ready():
-	pass 
+	pass
 
-func _physics_process( delta ):
+var last_damage_time = 0
+func _process( dt ):
+	last_damage_time += dt
+	
+	var ratio = health / max_health
+	$HUD/HealthRect.rect_scale.x = lerp( $HUD/HealthRect.rect_scale.x, ratio, dt * 4 )
+	if last_damage_time > .5:
+		$HUD/HealthRectDamage.rect_scale.x = lerp( $HUD/HealthRectDamage.rect_scale.x, ratio, dt * 5 )
+
+func _physics_process( dt ):
 	var dir = Vector2()
 	
 	if Input.is_action_pressed( "up" ):
@@ -31,3 +42,9 @@ func _physics_process( delta ):
 	#  move
 	move_and_slide( dir.normalized() * speed )
 	$AnimatedSprite.play( "walk" )
+
+func take_damage( damage: int ):
+	health -= damage
+	last_damage_time = 0
+	if health < 0:
+		get_tree().reload_current_scene()
